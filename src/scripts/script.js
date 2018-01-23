e1ecb7b9e3f646d7821497a6a9bd343f923f5067
NiKole Maxwell <nvymaxwell@outlook.com>
2018-01-23 01:33:23 -0500

    A | Slider Style pt 1

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,7 +5,6 @@
 const nvy = console.log("This project is just beginning to be nvy'd. Consult the Readme and project instructions for the steps to make fulfill the nvy.")
 
 
-nvy(); 
-
+nvy;
 
 /*/ Slider Script /*/
